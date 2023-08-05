cbc863db7a779a0a65ba98feae41f7eb1a7907f3
Dinar <dinar.fahriev@yandex.ru>
2023-08-05 19:03:23 +0300

    fix first scene

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,6 +5,12 @@ public class SceneController : MonoBehaviour
 {
     [SerializeField] private int currentSceneIndex;
 
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void OnBeforeSceneLoadRuntimeMethod()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     public void RestartScene()
     {
         SceneManager.LoadScene(currentSceneIndex);
