e270110951cf8287dedc43bd39a2858c979fc457
Dinar <dinar.fahriev@yandex.ru>
2023-08-06 14:33:31 +0300

    spawn food on Z

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,14 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [Header("Foods")]
     [SerializeField] GameObject[] _foodPrefabs;
 
+    [Header("X Position")]
     [SerializeField] private Transform minimalXTransform;
     [SerializeField] private Transform maximalXTransform;
 
+    [Header("Z Position")]
+    [SerializeField] private Transform minimalZTransform;
+    [SerializeField] private Transform maximalZTransform;
+
+    [Header("Spawn Parametres")]
     [SerializeField] private float _spawnPosY = 10;
     [SerializeField] private float _startDelay = 1;
     [SerializeField] private float _spawnInterval = 1;
@@ -22,8 +27,9 @@ public class SpawnManager : MonoBehaviour
     {
         int foodIndex = Random.Range(0, _foodPrefabs.Length);
         float xPosition = Random.Range(minimalXTransform.position.x, maximalXTransform.position.x);
+        float zPosition = Random.Range(minimalZTransform.position.z, maximalZTransform.position.z);
 
-        Vector3 spawnPos = new Vector3(xPosition, _spawnPosY, 0);
+        Vector3 spawnPos = new Vector3(xPosition, _spawnPosY, zPosition);
         Instantiate(_foodPrefabs[foodIndex], spawnPos, _foodPrefabs[foodIndex].transform.rotation);
     }
 }
