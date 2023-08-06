6fbce08a5d7ebb354550059f8157d900d28bc115
Dinar <dinar.fahriev@yandex.ru>
2023-08-06 11:42:49 +0300

    add level part 4

---
+++ Assets/Scripts/Player/PlayerEquip.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerEquip: MonoBehaviour
+{
+    [SerializeField] private Transform equipBone;
+
+    private EquipItem _equipedItem;
+
+    public void Equip(EquipItem item)
+    {
+        if (_equipedItem != null)
+            _equipedItem.UnEquip();
+
+        item.transform.parent = equipBone;
+        item.transform.ResetLocal();
+        _equipedItem = item;
+    }
+}
--- Assets/Scripts/Player/PlayerHibox.cs
+++ Assets/Scripts/Player/PlayerHibox.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class PlayerHibox : MonoBehaviour
 {
     [SerializeField] private Player player;
+    [SerializeField] private PlayerEquip playerEquip;
 
     public Player Player => player;
-    
+    public PlayerEquip PlayerEquip => playerEquip;
+
     public void Deactivate()
     {
         gameObject.SetActive(false);
